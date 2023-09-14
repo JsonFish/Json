4ce434eae96cbdfea880378f274657e7bf1028d6
Yuyu <yjq13938373146@163.com>
2023-09-14 23:51:16 +0800

    test

--- src/components/Layout/Main/index.vue
+++ src/components/Layout/Main/index.vue
@@ -13,7 +13,7 @@
 .main {
     width: 100%;
     height: 1000px;
-    background-image: linear-gradient(to right, #a37f7f 0%, #527c6d 19%, #3d7482 42%, #796c92 79%, #5a8f9b 100%);
+    // background-image: linear-gradient(to right, #a37f7f 0%, #527c6d 19%, #3d7482 42%, #796c92 79%, #5a8f9b 100%);
     padding-top: 50px;
 }
 </style>
