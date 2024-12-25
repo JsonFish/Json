17fcb551acba28f34961f9ccd863c9632a1a4f59
json <1557392527@qq.com>
2024-12-26 00:08:29 +0800

    perf: 删除多余代码和资源

--- src/api/category/index.ts
+++
@@ -1,8 +0,0 @@
-import request from "@/utils/request";
-import { BasicResponse, CategoryInfo } from "./type";
-enum API {
-    CategoryList = "/categoryList"
-}
-export const getCategory = () => {
-    return request.get<any, BasicResponse<CategoryInfo[]>>(API.CategoryList);
-};
--- src/api/image/type.ts
+++
@@ -1,5 +0,0 @@
-export interface BasicResponse<T = any> {
-    code: number
-    data: T
-    message: string
-}
