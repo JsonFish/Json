26ac9bbd53d6912114665088645b7d8f0bfd46d9
json <1557392527@qq.com>
2025-01-09 01:31:33 +0800

    feat: 修改footer组件

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -2,7 +2,7 @@ import { defineComponent } from 'vue'
 import Header from './Header/index.vue'
 import Main from './Main/index.vue'
 import NoiseBg from '@/components/NoiseBg/index.vue'
-import Footer from './Footer/index.vue'
+import Footer from './Footer/index.tsx'
 
 export default defineComponent({
   name: 'Layout',
