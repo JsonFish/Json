01ae81a9c40308fd1055965a3111446716ab3fff
json <1557392527@qq.com>
2025-01-21 21:16:36 +0800

    feat: 修改文件名

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,8 +1,8 @@
 import { defineComponent } from 'vue'
-import Header from './header/index.tsx'
-import Main from './main/index.tsx'
+import Header from './Header/index.tsx'
+import Main from './Main/index.tsx'
 import NoiseBg from '@/components/NoiseBg/index.vue'
-import Footer from './footer/index.tsx'
+import Footer from './Footer/index.tsx'
 
 export default defineComponent({
   name: 'Layout',
